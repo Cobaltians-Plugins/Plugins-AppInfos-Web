473a6dd68949fd9cec7bb8067ed8ebe32ad97ee8
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 15:23:16 +0200

    removing plugin options

--- cobalt.appInfos.js
+++ cobalt.appInfos.js
@@ -2,24 +2,10 @@
     var plugin={
         name:"appInfos",
 
-        init:function(options){
+        init:function(){
             //create shortcuts
             cobalt.getAppInfos = this.getAppInfos.bind(this);
-			
-            if (options){
-                this.config(options);
-            }
-
         },
-        config:function(settings){
-        },
-        /*
-            Callback is called with an object of app informations. This object contains :
-            {
-                versionName : "1.5.2" //the public verbose version number of the app.
-                versionCode : 4, //the build number of the app (for app submissions).
-            }
-         */
         getAppInfos:function(callback){
             this.send("getAppInfos", {}, function( data ){
 				if (typeof callback == "function"){
